79fd203ce9fb701229ae46f4192a4d83ddb3f6cf
Kanishka1005goyal <kanishkagoyal181@gmail.com>
2025-02-26 22:45:04 +0530

    Initial commit - React app with JSON data

--- med.js
+++ med.js
@@ -3,3 +3,7 @@ let p= document.querySelector("p");
 p.addEventListener("click",function(){
     console.log("paragragh clicked");
 });
+const { AptosClient } = require("@aptos-labs/ts-sdk");
+
+console.log("Aptos SDK loaded successfully!");
+
